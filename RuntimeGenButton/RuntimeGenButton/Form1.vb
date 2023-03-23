a2b31c8682763ab807ddcc84ce0fc1f6d0c4fc4c
0xtanmaye <106451224+0xtanmaye@users.noreply.github.com>
2023-03-23 10:33:21 +0530

    Delete RuntimeGenButton/RuntimeGenButton directory

--- RuntimeGenButton/RuntimeGenButton/Form1.vb
+++
@@ -1,8 +0,0 @@
-﻿Public Class Form1
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim btn As New Button
-        btn.Text = "New Button"
-        btn.Location = New Point(20, 150)
-        Me.Controls.Add(btn)
-    End Sub
-End Class
